ff1dbf43420757855cee1dd2a3bf615c38f9783d
Alexey Chu <alexeychu@gmail.com>
2014-09-03 22:17:54 +0400

    up preset tempetarure

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -476,7 +476,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define PLA_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 235
-#define ABS_PREHEAT_HPB_TEMP 120
+#define ABS_PREHEAT_HPB_TEMP 130
 #define ABS_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 //LCD and SD support
